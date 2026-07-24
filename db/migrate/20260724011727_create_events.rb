0c439b2091ff4c74ae51687083bf3829d4b2a556
Toyoda Takeru <takerun20040214@gmail.com>
2026-07-24 10:32:07 +0900

    add:eventモデルとコントローラを追加
    検証用の住所や緯度経度を登録する土台を追加しました

---
+++ db/migrate/20260724011727_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[7.2]
+  def change
+    create_table :events do |t|
+      t.text :name
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
